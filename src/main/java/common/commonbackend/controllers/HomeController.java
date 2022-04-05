24270de952d918a6ab4cbd759c70e69079350cf5
Zuzanna Santorowska <zuzannasantorowska@gmail.com>
2022-04-05 12:25:06 +0200

    add get by id

--- src/main/java/common/commonbackend/controllers/HomeController.java
+++ src/main/java/common/commonbackend/controllers/HomeController.java
@@ -1,18 +1,34 @@
 package common.commonbackend.controllers;
 
+import common.commonbackend.entities.Task;
+import common.commonbackend.repositories.TaskRepository;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Log4j2
 @Controller
 public class HomeController {
 
+    private final TaskRepository taskRepository;
+
+    public HomeController(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
     @RequestMapping("/")
     public ResponseEntity<String> home() {
         return new ResponseEntity<>("home", HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/task", method = RequestMethod.GET)
+    public ResponseEntity<Task> getTaskById(@RequestParam Long id) {
+        Task task = taskRepository.getTaskById(id);
+        return new ResponseEntity<>(task, HttpStatus.OK);
     }
 
 }

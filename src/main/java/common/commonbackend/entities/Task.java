b07a70647f7b0b9d88bd53397ca8f03c22011122
Zuzanna Santorowska <zuzannasantorowska@gmail.com>
2022-04-05 12:25:06 +0200

    add get by id

---
+++ src/main/java/common/commonbackend/entities/Task.java
@@ -0,0 +1,32 @@
+package common.commonbackend.entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.Objects;
+import java.util.Set;
+
+@RequiredArgsConstructor
+@NoArgsConstructor(force = true)
+@Entity
+@Getter
+@Table(name = "TASK")
+public class Task {
+    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID")
+    private final Long id;
+
+    @Column(name = "NAME")
+    private final String name;
+
+
+}
--- src/test/java/common/commonbackend/controllers/HomeControllerTest.java
+++ src/test/java/common/commonbackend/controllers/HomeControllerTest.java
@@ -1,12 +1,19 @@
 package common.commonbackend.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import common.commonbackend.entities.Task;
+import common.commonbackend.repositories.TaskRepository;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -19,10 +26,38 @@ public class HomeControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @org.junit.Test
+    @MockBean
+    private TaskRepository taskRepository;
+
+    @Test
     public void home() throws Exception {
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("home"));
+    }
+
+    @Test
+    public void shouldGetTaskById() throws Exception {
+        //given
+        Long taskId = 42L;
+        Task task = new Task(taskId,"name");
+
+        //when
+        when(taskRepository.getTaskById(taskId)).thenReturn(task);
+
+        //then
+        mockMvc.perform(get("/task?id=42"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(asJsonString(task)))
+                .andDo(print());
+
+    }
+
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }

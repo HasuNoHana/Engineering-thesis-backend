aac1bc19cdb05e364bfe8211e9ab73a20ece95c3
Zuzanna Santorowska <zuzannasantorowska@gmail.com>
2022-04-05 16:59:47 +0200

    add testing of repository

--- src/main/java/common/commonbackend/entities/Task.java
+++ src/main/java/common/commonbackend/entities/Task.java
@@ -27,4 +27,17 @@ public class Task {
 
     @Column(name = "NAME")
     private final String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return Objects.equals(id, task.id) && Objects.equals(name, task.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
 }
--- src/test/java/common/commonbackend/CommonBackendApplicationTests.java
+++ src/test/java/common/commonbackend/CommonBackendApplicationTests.java
@@ -1,13 +1,62 @@
 package common.commonbackend;
 
+import common.commonbackend.entities.Task;
+import common.commonbackend.repositories.TaskRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class CommonBackendApplicationTests {
 
+	@Autowired
+	private TaskRepository taskRepository;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void shouldAddTaskToDatabase() {
+		//given
+		Task task = new Task(1L, "name");
+
+		//when
+		taskRepository.save(task);
+
+		//then
+		assertThat(taskRepository.count()).isEqualTo(1);
+//		assertThat(taskRepository.getTaskById(1L));
+	}
+
+	@Test
+	void shouldAddAndDeleteTask() {
+		//given
+		Task task = new Task(1L, "name");
+
+		//when
+		taskRepository.save(task);
+
+		//then
+		assertThat(taskRepository.count()).isEqualTo(1);
+		taskRepository.delete(task);
+		assertThat(taskRepository.count()).isEqualTo(0);
+	}
+
+	@Test
+	void shouldSaveOnAlreadyExistingTask() {
+		//given
+		Task task = new Task(1L, "name");
+		taskRepository.save(task);
+
+		//when
+		Task task1 = new Task(1L, "name1");
+		taskRepository.save(task1);
+
+		//then
+		assertThat(taskRepository.count()).isEqualTo(1);
+		assertThat(taskRepository.getTaskById(1L)).isEqualTo(task1);
 	}
 
 }

29ca41e286f0689066743da37edccbfaf551b347
Zuzanna Santorowska <zuzannasantorowska@gmail.com>
2022-04-06 18:25:45 +0200

    test pipeline2

--- src/main/java/common/commonbackend/CommonBackendApplication.java
+++ src/main/java/common/commonbackend/CommonBackendApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CommonBackendApplication {
 
-	public static void main(String[] args) {
+	public static  void main(String[] args) {
 		SpringApplication.run(CommonBackendApplication.class, args);
 	}
 

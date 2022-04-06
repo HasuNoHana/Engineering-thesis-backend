e6b72e94d04333af319a938a0809428c65a10031
Zuzanna Santorowska <zuzannasantorowska@gmail.com>
2022-04-06 18:26:23 +0200

    tes

--- src/main/java/common/commonbackend/CommonBackendApplication.java
+++ src/main/java/common/commonbackend/CommonBackendApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CommonBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CommonBackendApplication.class, args);
+		SpringApplication.run(CommonBackendApplication.class,   args);
 	}
 
 }

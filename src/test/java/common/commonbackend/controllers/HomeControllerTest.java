dba01517eda51a7434c04fd195df3c50cd070001
Zuzanna Santorowska <zuzannasantorowska@gmail.com>
2022-04-07 11:35:42 +0200

    add testing post

--- src/test/java/common/commonbackend/controllers/HomeControllerTest.java
+++ src/test/java/common/commonbackend/controllers/HomeControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -47,6 +48,21 @@ public class HomeControllerTest {
         mockMvc.perform(get("/task?id=42"))
                 .andExpect(status().isOk())
                 .andExpect(content().json(asJsonString(task)))
+                .andDo(print());
+    }
+
+    @Test
+    public void shouldPostTask() throws Exception {
+        //given
+        Long taskId = 42L;
+        Task task = new Task(taskId, "name");
+
+        //when
+        when(taskRepository.getTaskById(taskId)).thenReturn(task);
+
+        //then
+        mockMvc.perform(post("/task?id=42&name=name"))
+                .andExpect(status().isOk())
                 .andDo(print());
     }
 

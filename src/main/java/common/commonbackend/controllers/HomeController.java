080969f9dcd8fb751c9dd3140e7bd1b0e3970d31
Zuzanna Santorowska <zuzannasantorowska@gmail.com>
2022-04-07 11:17:11 +0200

    fix code smells

--- src/main/java/common/commonbackend/controllers/HomeController.java
+++ src/main/java/common/commonbackend/controllers/HomeController.java
@@ -6,6 +6,9 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,25 +27,25 @@ public class HomeController {
         this.taskRepository = taskRepository;
     }
 
-    @RequestMapping("/")
+    @GetMapping(path = "/")
     public ResponseEntity<String> home() {
         return new ResponseEntity<>("home", HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/task", method = RequestMethod.GET)
+    @GetMapping(path = "/task")
     public ResponseEntity<Task> getTaskById(@RequestParam Long id) {
         Task task = taskRepository.getTaskById(id);
         return new ResponseEntity<>(task, HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/task", method = RequestMethod.POST)
+    @PostMapping(path = "/task")
     public ResponseEntity<Task> createOrUpdateTask(@RequestParam Map<String, String> params) {
         Task task = new Task(parseLong(params.get("id")), params.get("name"));
         taskRepository.save(task);
         return new ResponseEntity<>(task, HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/task", method = RequestMethod.DELETE)
+    @DeleteMapping(path = "/task")
     public ResponseEntity<String> deleteTask(@RequestParam Map<String, String> params) {
         taskRepository.delete(taskRepository.getTaskById(parseLong(params.get("id"))));
         return new ResponseEntity<>(params.get("id"), HttpStatus.OK);

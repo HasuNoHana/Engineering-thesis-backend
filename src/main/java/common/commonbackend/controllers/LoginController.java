0b4de6ecce31e444e420466433d0f6d5ebed5999
Zuzanna Santrowska <37709990+HasuNoHana@users.noreply.github.com>
2022-12-26 09:01:37 +0100

    Add create user (#17)
    * Fix house entity
    
    * Add create user endpoint

--- src/main/java/common/commonbackend/controllers/LoginController.java
+++ src/main/java/common/commonbackend/controllers/LoginController.java
@@ -1,5 +1,7 @@
 package common.commonbackend.controllers;
+import common.commonbackend.user.UserService;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
@@ -9,10 +11,19 @@ import java.security.Principal;
 @RequestMapping("/api")
 public class LoginController {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/user")
     @ResponseBody
     public Principal user(Principal user) {
         return user;
+    }
+
+    @PostMapping("/createUser")
+    public void createUser(@RequestBody String username, @RequestBody String password) {
+        log.info("Creating user with username: " + username + " and password: " + password);
+        userService.createUser(username, password);
     }
 
 }

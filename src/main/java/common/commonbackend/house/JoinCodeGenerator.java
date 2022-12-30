baf8cb9eef283669d1238e23fe45145b37455663
Zuzanna Santorowska <zuzannasantorowska@gmail.com>
2022-12-30 14:57:48 +0100

    add join code generator

--- src/main/java/common/commonbackend/house/JoinCodeGenerator.java
+++ src/main/java/common/commonbackend/house/JoinCodeGenerator.java
@@ -1,14 +1,17 @@
 package common.commonbackend.house;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Random;
 
 @Service
-@RequiredArgsConstructor
 public class JoinCodeGenerator {
-    private final Random random = new Random();
+
+    private final Random random;
+
+    public JoinCodeGenerator(Random random) {
+        this.random = random;
+    }
 
     public String generateNewJoinCode() {
         int generatedCode = this.random.nextInt(9000) + 1000; //range 1000-9999
--- src/test/java/common/commonbackend/house/JoinCodeGeneratorTest.java
+++ src/test/java/common/commonbackend/house/JoinCodeGeneratorTest.java
@@ -1,18 +1,26 @@
 package common.commonbackend.house;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoSettings;
+
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
 
 @MockitoSettings
 class JoinCodeGeneratorTest {
 
+    @Mock
+    private Random random;
+
     @Test
     void shouldGenerateNewJoinCode() {
         //given
-        JoinCodeGenerator joinCodeGenerator = new JoinCodeGenerator();
+        JoinCodeGenerator joinCodeGenerator = new JoinCodeGenerator(random);
+        when(random.nextInt(9000)).thenReturn(1000);
 
         //when
         String joinCode = joinCodeGenerator.generateNewJoinCode();

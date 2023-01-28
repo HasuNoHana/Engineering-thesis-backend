54bdd4292249f10c6896c6b941b1e06882ab8f26
Zuzanna Santorowska <zuzannasantorowska@gmail.com>
2023-01-28 13:23:26 +0100

    add database start script

--- start_db.sh
+++ start_db.sh
@@ -1,1 +1,7 @@
-docker run --name sql-common -e MYSQL_ROOT_PASSWORD=haslo -d mysql:latest
+#!/bin/bash
+docker-compose -f database/stack.yml up
+mysql -h localhost -P 3306 --protocol=tcp -u root -pdupa --execute="
+create database db_example;
+create user 'springuser'@'%' identified by 'ThePassword';
+grant all on db_example.* to 'springuser'@'%';
+"

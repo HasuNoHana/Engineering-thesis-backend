5f101604b0734556658035e1722861e91a52d63e
Zuzanna Santorowska <zuzannasantorowska@gmail.com>
2023-01-28 13:37:08 +0100

    wip

--- start_db.sh
+++ start_db.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
 docker-compose -f database/stack.yml up
-mysql -h localhost -P 3306 --protocol=tcp -u root -pdupa --execute="
-create database db_example;
-create user 'springuser'@'%' identified by 'ThePassword';
-grant all on db_example.* to 'springuser'@'%';
-"
